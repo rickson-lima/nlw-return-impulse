d1cb54cfe6e34345223f4a777b5bd7ab5e5df89c
Rickson Lima <rickson.lima@grupoicts.com.br>
2022-05-07 11:47:20 -0400

    chore: add server port config

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3002, () => {
+app.listen(process.env.PORT || 3002, () => {
   console.log("Server is running on port 3002 🚀");
 });
