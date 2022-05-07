f7dc77625743b8f215c8559ddf413b6933158a91
Rickson Lima <rickson.lima@grupoicts.com.br>
2022-05-07 16:37:30 -0400

    add mobile app

---
+++ mobile/src/components/Copyright/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {
+  Text,
+  View
+} from 'react-native';
+
+import { styles } from './styles';
+
+export function Copyright() {
+  return (
+    <View>
+      <Text style={styles.text}>
+        Feito com ❤️ por Rickson Lima
+      </Text>
+    </View>
+  );
+}
